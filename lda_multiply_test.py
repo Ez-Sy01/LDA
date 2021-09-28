bc43f43ed72232186f7b26a8b809e631f6155680
Ez-Sy01 <66240545+Ez-Sy01@users.noreply.github.com>
2021-09-29 02:43:37 +0900

    테스트도 완벽함.

--- lda_multiply_test.py
+++ lda_multiply_test.py
@@ -9,7 +9,7 @@ E_cls1 = np.mean(cls1_data,axis = 0)
 E_cls2 = np.mean(cls2_data,axis = 0)
 E_all = (np.sum(cls1_data,axis = 0) + np.sum(cls2_data, axis = 0)) / (np.size(cls1_data, 0)  +np.size(cls2_data,0))
 
-plt.figure('data - variance - draw')
+# plt.figure('data - variance - draw')
 plt.axis([0,10,0,10])
 plt.plot(cls1_data[:,0],cls1_data[:,1], '*r')
 plt.plot(cls2_data[:,0],cls2_data[:,1], '*b')
@@ -38,8 +38,8 @@ value_index = np.argmax(eig_val)
 vector = eig_vec[:,value_index]
 
 # this vector space projection
-new_cls1_data = cls1_data * vector
-new_cls2_data = cls2_data * vector
+new_cls1_data = np.dot(cls1_data , np.transpose([vector]))
+new_cls2_data = np.dot(cls2_data , np.transpose([vector]))
 
 # draw vector on graph
 a = vector[1] / vector[0]
@@ -57,37 +57,49 @@ for i in range(np.size(cls2_data, 0)):
     plt.plot(new_x,new_y, '*b')
     plt.plot([cls2_data[i,0],new_x], [cls2_data[i,1],new_y], '--k')
 
-# testing
-test_data = np.array([4.81,3.46])
-plt.plot(test_data[0],test_data[1],' *g')
-result = test_data * vector # this vector projection test_data
 
-projected_test_data_x = (test_data[0] + a * test_data[1]) / (a ** 2 + 1)
-projected_test_data_y = a * projected_test_data_x
-plt.plot(projected_test_data_x, projected_test_data_y,'*g')
-plt.plot([test_data[0], projected_test_data_x],[test_data[1], projected_test_data_y], '--k')
+# testing_1
+test_data1 = np.array([4.81,3.46])
+plt.plot(test_data1[0],test_data1[1],' *g')
+result = np.dot(test_data1 , np.transpose([vector])) # this vector projection test_data1
 
-# classification -> average in class
+projected_test_data1_x = (test_data1[0] + a * test_data1[1]) / (a ** 2 + 1)
+projected_test_data1_y = a * projected_test_data1_x
+plt.plot(projected_test_data1_x, projected_test_data1_y,'*g')
+plt.plot([test_data1[0], projected_test_data1_x],[test_data1[1], projected_test_data1_y], '--k')
+
+# classification -> average in class 1
 temp1 = new_cls1_data - result
 temp2 = new_cls2_data - result
-
-distance_result1 = list()
-for i in range(np.size(temp1,0)):
-    for j in range(i + 1,np.size(temp1,0)):
-        distance_result1.append(np.linalg.norm(temp1[i] - temp1[j]))
-distance_result1 = np.mean(distance_result1)
-print(distance_result1)
-
-distance_result2 = list()
-for i in range(np.size(temp2,0)):
-    for j in range(i + 1,np.size(temp2,0)):
-        distance_result2.append(np.linalg.norm(temp2[i] - temp2[j]))
-distance_result2 = np.mean(distance_result2)
-print(distance_result2)
-
-if distance_result1 < distance_result2:
+print(temp1)
+print(temp2)
+if np.min(np.abs(temp1))  < np.min(np.abs(temp2)):
     print('class -> red')
 else:
     print('class -> blue')
-plt.gca().set_title('experiment -> 2-D LDA Algorithm')
+
+
+
+
+# testing_2
+test_data2 = np.array([2.41,3.1])
+plt.plot(test_data2[0],test_data2[1],' *g')
+result = np.dot(test_data2 , np.transpose([vector])) # this vector projection test_data1
+
+projected_test_data2_x = (test_data2[0] + a * test_data2[1]) / (a ** 2 + 1)
+projected_test_data2_y = a * projected_test_data2_x
+plt.plot(projected_test_data2_x, projected_test_data2_y,'*g')
+plt.plot([test_data2[0], projected_test_data2_x],[test_data2[1], projected_test_data2_y], '--k')
+
+# classification -> average in class 1
+temp1 = new_cls1_data - result
+temp2 = new_cls2_data - result
+print(temp1)
+print(temp2)
+if np.min(np.abs(temp1))  < np.min(np.abs(temp2)):
+    print('class -> red')
+else:
+    print('class -> blue')
+    
+plt.gca().set_title('experiment -> 2-D LDA')
 plt.show()
